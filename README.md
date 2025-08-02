d6a71e2f67b4531f70d2a1a29431d8f8c8f8d564
ProgrammEverything <sasanxcoder@gmail.com>
2025-08-02 09:51:19 +0330

    added basic features and a basic readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# YouTube-utilities
+# Youtube Utilities
+## This repo is as of "currently" in the state of developement. Please be aware of security risks and/or bugs.
+Youtube Utilities contains many utilities based on command line tools. There is a GUI coming soon tho. Lookout for that.
---
+++ src/download_video.py
@@ -0,0 +1,8 @@
+###############################################################################
+#  -  IMPORTANT -                                                             #
+#    File: download_video.py                                                  #
+#    Description: DOWNLOAD_APPLICATION Youtube utility                        #
+#    Copyright (c) candleLight and all contributors.                          #
+#    Use of this source code is governed by the license found in the LICENSE  #
+#    file at the root of this repository.                                     #
+###############################################################################
